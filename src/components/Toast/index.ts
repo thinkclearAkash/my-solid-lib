c9096a347d903f6bf84d94e3a3cd0c7d71ff2ad8
Yatin kumar <yatinkumar@Yatins-MacBook-Pro.local>
2024-05-14 15:01:29 +0530

    minor chnage

--- src/components/Toast/index.ts
+++ src/components/Toast/index.ts
@@ -1,6 +1,4 @@
 import { ToastType, openToast } from './Toast';
 import { Toaster } from 'solid-toast';
 
-export { Toaster, openToast }
-
-export type { ToastType }
+export { Toaster, openToast, ToastType }
